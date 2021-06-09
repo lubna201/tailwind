8330090875f33102df05d1057cc728cdde6767e4
lubna201 <lubnasazida@gmail.com>
2021-06-09 21:20:45 +0600

    done

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,13 +3,13 @@ import data from '../data';
 import pic from '../1.png'
 
 const Projects = () => {
-  const [items, setItems] = useState(data)
+  const [items, ] = useState(data)
 
   return (
     <>
       <section className="py-20 px-5 bg-green-800 grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-3 lg:h-screen lg:pt-32">
         {items.map((item) => {
-          const { id, path, desc } = item
+          const { id,  desc } = item
           return (
             <div key={id}>
               <small className="block text-center text-white font-semibold text-lg">
